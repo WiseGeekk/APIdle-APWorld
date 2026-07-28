7cd44ad80e607bb6896f016a1e55d47132300cb7
Simon VIlslev <simon-vilslev@hotmail.com>
2026-07-28 13:54:52 +0200

    Generate APworld skeleton

---
+++ world/world.py
@@ -0,0 +1,46 @@
+from collections.abc import Mapping
+from typing import Any
+
+from worlds.AutoWorld import World
+
+from . import items, locations, regions, rules, web_world
+from . import options as APIdle_options
+
+
+class APIdleWorld(World):
+    """
+    APIdle is an idle game, with prestiges
+    """
+
+    game = "APIdle"
+
+    web = web_world.APIdleWebWorld()
+
+    
+    options_dataclass = APIdle_options.APIdleOptions
+    options: APIdle_options.APIdleOptions
+
+    location_name_to_id = locations.LOCATION_NAME_TO_ID
+    item_name_to_id = items.ITEM_NAME_TO_ID
+
+    origin_region_name = "Test1"
+
+    def create_regions(self) -> None:
+        regions.create_and_connect_regions(self)
+        locations.create_all_locations(self)
+
+    def set_rules(self) -> None:
+        rules.set_all_rules(self)
+
+    def create_items(self) -> None:
+        items.create_all_items(self)
+
+    def create_item(self, name: str) -> items.APIdleItem:
+        return items.create_item_with_correct_classification(self, name)
+
+    def get_filler_item_name(self) -> str:
+        return items.get_random_filler_item_name(self)
+
+    def fill_slot_data(self) -> Mapping[str, Any]:
+        return self.options.as_dict(
+        )
